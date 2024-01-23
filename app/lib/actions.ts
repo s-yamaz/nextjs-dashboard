70497a0c345c1eea17c3cd76c87ee58eb9c3f824
Shinya Yamazaki <s-yamaz@users.noreply.github.com>
2024-01-23 05:22:52 +0000

    Chapter 12: 6. Revalidate and redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
+import { revalidatePath } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -11,18 +13,20 @@ const FormSchema = z.object({
   date: z.string(),
 })
 
-const CreateInvoice = z.omit({ id: true, date: true, });
+const CreateInvoice = FormSchema.omit({ id: true, date: true, });
  
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = FormSchema.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInCentes = amount * 100;
+  const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCentes}, ${status}, ${date})
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
