831da56f2a889e21909abe511b0fec539565966a
Shinya Yamazaki <s-yamaz@users.noreply.github.com>
2024-01-23 04:39:53 +0000

    Chapter 12: 5. Inserting the data into your database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 
 const FormSchema = z.object({
@@ -20,4 +21,8 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCentes = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCentes}, ${status}, ${date})
+  `;
 }
