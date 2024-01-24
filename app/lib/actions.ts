ee3867e228def5bf22af9cb1c01bc2a47fe6d934
Shinya Yamazaki <s-yamaz@users.noreply.github.com>
2024-01-24 05:17:45 +0000

    fixes typos

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -50,9 +50,6 @@ export async function updateInvoice(id: string,formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
-    DELETE FROM invoices WHERE id = ${id}
-  `;
-
-  revalidateParh('/dashboard/invoices');
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
